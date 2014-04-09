c689b011be605f297f683622a8015aed87b8ea45
Michael Quinlan <mquinlan@sportslabs.com>
2014-04-08 22:08:26 -0600

    master: Making it so that heroku will run it properly

---
+++ web.js
@@ -0,0 +1,9 @@
+var gzippo = require('gzippo');
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 5000;
+app.use(gzippo.staticGzip('' + __dirname + '/dist'));
+app.listen(port);
+
+console.log('Listening on port: ' + port);
