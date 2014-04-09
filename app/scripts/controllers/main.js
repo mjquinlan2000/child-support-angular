ecb51fc4b3fc820a4e164d1169bc5d7cd3973a04
Michael Quinlan <mquinlan@sportslabs.com>
2014-04-08 21:09:01 -0600

    master: Adding support schedule. Determining support from the schedule

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,33 +1,46 @@
 'use strict';
 
 angular.module('childSupportApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.support = {
-      numChildren: 0,
-      numOvernights: 0,
-      maintenancePaid: 0,
-      maintenanceReceived: 0,
-      childOther: 0,
-      childSpouse: 0,
-      childExp: 0,
-      childExpSpouse: 0,
-      spouseIncome: 0,
-      income: 0
-    };
+  .controller('MainCtrl', function ($scope, SUPPORT_SCHEDULE_2014) {
 
-    $scope.calculated = {
-      spouseAdjustedIncome: 0,
-      adjustedIncome: 0,
-      combinedAdjustedIncome: 0
-    };
+    $scope.initialize = function() {
+      $scope.support = {
+        numChildren: 0,
+        numOvernights: 0,
+        maintenancePaid: 0,
+        maintenanceReceived: 0,
+        childOther: 0,
+        childSpouse: 0,
+        childExp: 0,
+        childExpSpouse: 0,
+        spouseIncome: 0,
+        income: 0
+      };
 
-    $scope.display = {
-      percentIncome: 0,
-      spousePercentIncome: 0
+
+      $scope.calculated = {
+        spouseAdjustedIncome: 0,
+        adjustedIncome: 0,
+        combinedAdjustedIncome: 0,
+        supportObligation: 0,
+        obligationShare: 0,
+        spouseObligationShare: 0,
+        percentageIncome: 0,
+        spousePercentageIncome: 0
+      };
+
+      $scope.display = {
+        percentIncome: 0,
+        spousePercentIncome: 0,
+        obligationShare: 0,
+        spouseObligationShare: 0
+      };
     };
 
     $scope.calculateSupport = function() {
       $scope.adjustedGrossIncome();
+      $scope.determineSchedule();
+      localStorage.setItem('support', JSON.stringify($scope.support));
     };
 
     $scope.adjustedGrossIncome = function() {
@@ -48,6 +61,31 @@ angular.module('childSupportApp')
       $scope.display.percentIncome = Math.round($scope.calculated.percentageIncome*100);
       $scope.display.spousePercentIncome = Math.round($scope.calculated.spousePercentageIncome*100);
     };
+
+    $scope.determineSchedule = function() {
+      var scheduleEntry,
+        numChildren;
+      _.each(SUPPORT_SCHEDULE_2014, function(row) {
+        if(row.income <= $scope.calculated.combinedAdjustedIncome) {
+          scheduleEntry = row;
+        }
+      });
+
+      numChildren = Math.min($scope.support.numChildren, 6);
+
+      $scope.calculated.supportObligation = scheduleEntry[numChildren];
+      $scope.calculated.obligationShare = $scope.calculated.supportObligation * $scope.calculated.percentageIncome;
+      $scope.calculated.spouseObligationShare = $scope.calculated.supportObligation * $scope.calculated.spousePercentageIncome;
+
+      $scope.display.obligationShare = $scope.calculated.obligationShare.toFixed(2);
+      $scope.display.spouseObligationShare = $scope.calculated.spouseObligationShare.toFixed(2);
+    };
+
+    $scope.initialize();
+
+    if(localStorage.getItem('support')) {
+      $scope.support = angular.extend($scope.support, JSON.parse(localStorage.getItem('support')));
+    }
 
     $scope.$watchCollection('support', $scope.calculateSupport);
   });
